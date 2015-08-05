2b18ad74cbc8048dff226215e518cedf70e357d8
yplam <yplam@yplam.com>
2015-08-05 13:18:29 +0800

    add wechat event

--- src/AppBundle/Controller/WechatController.php
+++ src/AppBundle/Controller/WechatController.php
@@ -5,8 +5,15 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
 use YPL\WechatSDK\Exception\WechatException;
-use Symfony\Component\HttpFoundation\Response;
+use YPL\WechatSDK\Model\Message\BaseMessage;
+use YPL\WechatSDK\Model\Message\BaseEvent;
+
+use AppBundle\Wechat\Response;
+use AppBundle\Wechat\WechatEvents;
+use AppBundle\Wechat\Event\MessageEvent;
+
 
 class WechatController extends Controller
 {
@@ -16,16 +23,33 @@ class WechatController extends Controller
     public function wechatAction(Request $request)
     {
         $wechat = $this->get('app_wechat');
+        $response = new Response('success');
+        $response->setWechat($wechat);
         try{
             $rawMessage = $wechat->getRawMessage();
             if($echostr = $request->query->get('echostr')){
-                return new Response($echostr);
+                $response->setContent($echostr);
+                return $response;
             }
-            $message = $this->get('app_wechat_message_manager')->createFromRawMessage($rawMessage);
-            return new Response('success');
+            $messageManager = $this->get('app_wechat_message_manager');
+            $message = $messageManager->createFromRawMessage($rawMessage);
+            if(!($message && $messageManager->validate($message))){
+                throw new WechatException('message validate error');
+            }
+            $event = new MessageEvent($message, $response);
+            $dispatcher = $this->get('event_dispatcher');
+            $dispatcher->dispatch(WechatEvents::MESSAGE, $event);
+            if($message instanceof BaseMessage){
+                $dispatcher->dispatch('wechat_'.$message->getMsgType(), $event);
+            }
+            else if($message instanceof BaseEvent){
+                $dispatcher->dispatch('wechat_event_'.strtolower($message->getEvent()), $event);
+            }
         }
         catch(WechatException $e){
-            exit('error');
+            $response->setContent('error');
+            $this->get('logger')->error('Wechat - ' . $e->getMessage());
         }
+        return $response;
     }
 }
---
+++ src/AppBundle/Wechat/Response.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This file is part of the WechatEdition package.
+ *
+ * (c) yplam <yplam@yplam.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace AppBundle\Wechat;
+
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use YPL\WechatSDK\Model\ResponseInterface;
+use YPL\WechatSDK\Wechat;
+
+
+class Response extends HttpResponse
+{
+
+    protected $responseMessage;
+
+    protected $wechat = null;
+
+    public function setWechat(Wechat $wechat)
+    {
+        $this->wechat = $wechat;
+    }
+
+    public function getResponseMessage()
+    {
+        return $this->responseMessage;
+    }
+
+    public function hasResponseMessage()
+    {
+        return !empty($this->responseMessage);
+    }
+
+    public function setResponseMessage(ResponseInterface $responseMessage){
+        $this->responseMessage = $responseMessage;
+    }
+
+    public function __toString()
+    {
+        if($this->wechat && $this->hasResponseMessage()){
+            return
+                sprintf('HTTP/%s %s %s', $this->version, $this->statusCode, $this->statusText)."\r\n".
+                $this->headers."\r\n".
+                $wechat->response($this->getResponseMessage()->getRawResponse());
+        }
+        return parent::__toString();
+    }
+}
+    
