2e8c8554fb036ee59ae532c2dc34d1e6252deafc
yplam <yplam@yplam.com>
2015-08-21 13:27:08 +0800

    remove sonata admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,11 +23,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-            new Sonata\CoreBundle\SonataCoreBundle(),
-            new Sonata\BlockBundle\SonataBlockBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-            new Sonata\AdminBundle\SonataAdminBundle(),
             new YPL\WechatBundle\YPLWechatBundle(),
         );
 
